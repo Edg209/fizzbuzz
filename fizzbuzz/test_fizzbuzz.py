7013b18479605c645c354b79d48c4d0f2fb19385
Ed <Edgrant209@googlemail.com>
2022-10-29 14:06:27 +0100

    Added empty tests

--- fizzbuzz/test_fizzbuzz.py
+++ fizzbuzz/test_fizzbuzz.py
@@ -4,4 +4,18 @@ raise NotImplementedError
 
 
 class TestFizzBuzzGenerator(TestCase):
-    raise NotImplementedError
+    def test_negative_integer(self):
+        """Test that attempting to replace a negative integer raises the appropriate exception"""
+        raise NotImplementedError
+
+    def test_non_integer(self):
+        """Test that attempting to replace a non integer raises the appropriate exception"""
+        raise NotImplementedError
+
+    def test_default_replacements(self):
+        """Test that the default replacements of {3:Fizz, 5:Buzz} correctly generates the first 100 terms"""
+        raise NotImplementedError
+
+    def test_empty_replacements(self):
+        """Test that a FizzBuzzGenerator with an empty replacements dict correctly generates the first 100 terms"""
+        raise NotImplementedError
